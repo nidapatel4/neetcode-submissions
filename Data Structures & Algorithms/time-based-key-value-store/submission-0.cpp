fd6c3f71e65786450be659bb00d1b281dc99ff1c
nidapatel4 <163721013+nidapatel4@users.noreply.github.com>
2026-07-12 04:24:50 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,30 @@
+class TimeMap {
+private:
+    unordered_map<string, vector<pair<int,string>>> mp;
+
+public:
+    TimeMap() {
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({timestamp,value});
+    }
+    
+    string get(string key, int timestamp) {
+        vector<pair<int,string>>& vec = mp[key];
+        int low=0;
+        int high=vec.size()-1;
+        string ans="";
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(vec[mid].first<=timestamp){
+                ans=vec[mid].second;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
